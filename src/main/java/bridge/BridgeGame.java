dad97a079a3ac4cd1ea7f46b8168e7e614c6a2ee
GaBaljaintheroom <junsu1222@naver.com>
2023-08-12 01:11:14 +0900

    refactor: move 매개변수 명 수정

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -20,8 +20,8 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public boolean move(String bridgeMoveStep, int count) {
-        String floor = bridge.get(count);
+    public boolean move(String bridgeMoveStep, int stage) {
+        String floor = bridge.get(stage);
         return floor.equals(bridgeMoveStep);
     }
 
