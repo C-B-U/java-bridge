1c65a5e3361bdbef67a07294408e488eed2cba64
GaBaljaintheroom <junsu1222@naver.com>
2023-08-12 01:14:28 +0900

    refactor: InputValidateTest 수정

--- src/test/java/bridge/InputValidateTest.java
+++ src/test/java/bridge/InputValidateTest.java
@@ -7,19 +7,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputValidateTest {
 
-    InputView inputView = new InputView();
+    InputValidator inputValidator = new InputValidator();
+
 
     @Test
     @DisplayName("다리의 길이가 3~20사이의 숫자가 아니면 에러가 발생한다.")
     void validateBridgeSize(){
-        assertThatThrownBy(() -> inputView.inputBridgeSize("1"))
+        assertThatThrownBy(() -> inputValidator.validateBridgeSize("1"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("이동할 칸이 'U' 또는 'D'가 아니면 에러가 발생한다.")
     void validateBridgeMoveStep(){
-        assertThatThrownBy(() -> inputView.inputBridgeMoveStep("Z"))
+        assertThatThrownBy(() -> inputValidator.validateBridgeMoveStep("Z"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+
 }
