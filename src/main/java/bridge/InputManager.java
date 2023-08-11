bd054be0260ab8f41cd81cfaf2f111552c7cc66e
GaBaljaintheroom <junsu1222@naver.com>
2023-08-11 23:14:28 +0900

    feat: 다리 이동 메서드 작성

--- src/main/java/bridge/InputManager.java
+++ src/main/java/bridge/InputManager.java
@@ -1,23 +1,40 @@
 package bridge;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import java.util.List;
 
 public class InputManager {
 
     private final InputView inputView;
     private final BridgeMaker bridgeMaker;
+    private final BridgeStatus bridgeStatus;
 
-    public InputManager(InputView inputView, BridgeMaker bridgeMaker) {
-        this.inputView = inputView;
-        this.bridgeMaker = bridgeMaker;
+    public InputManager() {
+        this.inputView = new InputView();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridgeStatus = new BridgeStatus();
     }
 
     public List<String> makeBridge(){
+        System.out.println("다리의 길이를 입력해주세요.");
         int bridgeSize = inputView.inputBridgeSize(Console.readLine());
         return bridgeMaker.makeBridge(bridgeSize);
     }
+
+    public void moveBridge(BridgeGame bridgeGame){
+        int count = 0;
+        while (true){
+            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+            String bridgeMoveStep = inputView.inputBridgeMoveStep(Console.readLine());
+            boolean isProceed = bridgeGame.move(bridgeMoveStep, count);
+            if (!isProceed){
+                bridgeStatus.failStair(bridgeMoveStep);
+                bridgeStatus.reset();
+            }
+            bridgeStatus.successStair(bridgeMoveStep);
+        }
+    }
+
 
 
 
