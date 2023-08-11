de9e388655e8f2769c8873e33287d2ee0b6a4355
GaBaljaintheroom <junsu1222@naver.com>
2023-08-11 23:13:12 +0900

    feat: Bridge 성공 메서드 작성

---
+++ src/main/java/bridge/BridgeStatus.java
@@ -0,0 +1,50 @@
+package bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeStatus {
+
+    private final List<BridgeWindow> upStair = new ArrayList<>();
+    private final List<BridgeWindow> downStair = new ArrayList<>();
+
+    BridgeStatus(){
+        init();
+    }
+
+    private void init(){
+        upStair.add(BridgeWindow.START);
+        downStair.add(BridgeWindow.START);
+    }
+
+    public void reset() {
+        upStair.clear();
+        downStair.clear();
+        init();
+    }
+
+    public void successStair(String bridgeMoveStep) {
+        if (bridgeMoveStep.equals("U")){
+            upStair.add(BridgeWindow.SUCCESS);
+            inputDivisionOrEnd(upStair);
+            downStair.add(BridgeWindow.BLANK);
+            inputDivisionOrEnd(downStair);
+        }
+        upStair.add(BridgeWindow.BLANK);
+        inputDivisionOrEnd(upStair);
+        downStair.add(BridgeWindow.SUCCESS);
+        inputDivisionOrEnd(downStair);
+    }
+
+
+
+    private void inputDivisionOrEnd(List<BridgeWindow> stair){
+        if (stair.contains(BridgeWindow.END)){
+            stair.remove(stair.size()-1);
+            stair.add(BridgeWindow.DIVISION);
+        }
+    }
+
+
+}
+
