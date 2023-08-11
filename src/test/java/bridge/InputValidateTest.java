d84dc86bfb3c5916e4848037a4584839f1c43684
GaBaljaintheroom <junsu1222@naver.com>
2023-08-11 16:58:13 +0900

    test: 이동할 칸이 'U' 또는 'D'가 아니면 에러

--- src/test/java/bridge/InputValidateTest.java
+++ src/test/java/bridge/InputValidateTest.java
@@ -7,11 +7,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputValidateTest {
 
+    InputView inputView = new InputView();
+
     @Test
     @DisplayName("다리의 길이가 3~20사이의 숫자가 아니면 에러가 발생한다.")
     void validateBridgeSize(){
-        InputView inputView = new InputView();
         assertThatThrownBy(() -> inputView.inputBridgeSize("1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이동할 칸이 'U' 또는 'D'가 아니면 에러가 발생한다.")
+    void validateBridgeMoveStep(){
+        assertThatThrownBy(() -> inputView.inputBridgeMoveStep("Z"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
