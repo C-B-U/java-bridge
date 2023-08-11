1a8b32e814dccdc048921076a9dc11b07dd5cc07
GaBaljaintheroom <junsu1222@naver.com>
2023-08-11 16:56:21 +0900

    feat: 입력할 칸을 입력 메서드 작성

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,8 +11,10 @@ public class InputView {
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public String readMoving() {
-        return null;
+    public String inputBridgeMoveStep(String bridgeMoveStep) {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        inputValidator.validateBridgeMoveStep(bridgeMoveStep);
+        return bridgeMoveStep;
     }
 
     /**
