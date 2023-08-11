b1d017b074d2de487b10c203c5e2209650cc77bf
GaBaljaintheroom <junsu1222@naver.com>
2023-08-11 23:50:18 +0900

    feat: 게임 다시 시도할지 여부 입력 validate 작성

--- src/main/java/bridge/InputValidator.java
+++ src/main/java/bridge/InputValidator.java
@@ -8,10 +8,16 @@ public class InputValidator {
             throw new IllegalArgumentException("[ERROR] 다리의 길이는 3~20 사이의 숫자입니다.");
         }
     }
-
-            public void validateBridgeMoveStep(String bridgeMoveStep) {
+    public void validateBridgeMoveStep(String bridgeMoveStep) {
         if (!bridgeMoveStep.equals("U") && !bridgeMoveStep.equals("D")){
             throw new IllegalArgumentException("[Error] 이동할 칸은 'U' 혹은 'D' 입니다.");
         }
     }
+
+    public void validateBridgeStartCommand(String gameCommand) {
+        if (!gameCommand.equals("R") && !gameCommand.equals("Q")){
+            throw new IllegalArgumentException("[Error] 게임을 다시 시도할 명령어는 'R' 혹은 'Q' 입니다.");
+        }
+    }
+
 }
