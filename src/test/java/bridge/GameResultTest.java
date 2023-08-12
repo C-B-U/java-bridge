c8f59c5a897eec9c2e03b81b05a198de4a9560d8
GaBaljaintheroom <junsu1222@naver.com>
2023-08-12 16:48:39 +0900

    test: 게임 클리어 실패시 '성공'에서 '실패'로 변하는지 확인

--- src/test/java/bridge/GameResultTest.java
+++ src/test/java/bridge/GameResultTest.java
@@ -25,10 +25,13 @@ class GameResultTest {
     @Test
     @DisplayName("윗 칸을 누르고 실패했을 때 게임 화면이 만들어지는지 확인한다.")
     void upStairFail(){
+        //given
         bridgeStatus.failStair("U");
 
+        //when
         String upStairSuccess = bridgeStatus.toString();
 
+        //then
         assertThat(upStairSuccess).contains("[ X ]\n[   ]\n");
 
     }
@@ -36,10 +39,13 @@ class GameResultTest {
     @Test
     @DisplayName("아랫 칸을 누르고 실패했을 때 게임 화면이 만들어지는지 확인한다.")
     void downStairFail(){
+        //given
         bridgeStatus.failStair("D");
 
+        //when
         String upStairSuccess = bridgeStatus.toString();
 
+        //then
         assertThat(upStairSuccess).contains("[   ]\n[ X ]\n");
 
     }
@@ -47,10 +53,13 @@ class GameResultTest {
     @Test
     @DisplayName("윗 칸을 누르고 성공했을 때 게임 화면이 만들어지는지 확인한다.")
     void upStairSuccess(){
+        //given
         bridgeStatus.successStair("U");
 
+        //when
         String upStairSuccess = bridgeStatus.toString();
 
+        //then
         assertThat(upStairSuccess).contains("[ O ]\n[   ]\n");
 
     }
@@ -58,12 +67,25 @@ class GameResultTest {
     @Test
     @DisplayName("아랫 칸을 누르고 성공했을 때 게임 화면이 만들어지는지 확인한다.")
     void downStairSuccess(){
+        //given
         bridgeStatus.successStair("D");
 
+        //when
         String upStairSuccess = bridgeStatus.toString();
 
+        //then
         assertThat(upStairSuccess).contains("[   ]\n[ O ]\n");
 
+    }
+
+    @Test
+    @DisplayName("게임 클리어 실패시 '성공'에서 '실패'로 변하는지 확인한다.")
+    void gameClearFail(){
+        assertThat(gameResult.getGameClear()).isEqualTo("성공");
+
+        gameResult.gameClearFail();
+
+        assertThat(gameResult.getGameClear()).isEqualTo("실패");
     }
 
 
