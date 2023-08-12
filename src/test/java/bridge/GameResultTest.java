d568fbce66de5471b66854157871e2b7abc118d9
GaBaljaintheroom <junsu1222@naver.com>
2023-08-12 15:58:05 +0900

    test: 윗 칸을 누르고 성공했을 때 게임 화면이 만들어지는지 확인

--- src/test/java/bridge/GameResultTest.java
+++ src/test/java/bridge/GameResultTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 class GameResultTest {
 
     GameResult gameResult = new GameResult();
+    BridgeStatus bridgeStatus = new BridgeStatus();
 
     @Test
     @DisplayName("게임 시도 횟수가 하나씩 증가하는지 확인한다.")
@@ -20,4 +21,17 @@ class GameResultTest {
         //then
         assertThat(attemptCount).isEqualTo(1);
     }
+
+    @Test
+    @DisplayName("윗 칸을 누르고 성공했을 때 게임 화면이 만들어지는지 확인한다.")
+    void upStairSuccess(){
+        bridgeStatus.failStair("U");
+
+        String upStairSuccess = bridgeStatus.toString();
+
+        assertThat(upStairSuccess).contains("[ X ]\n[   ]\n");
+
+    }
+
+
 }
