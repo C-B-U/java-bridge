895a59f86b3b01c7c90b9d200f2965ec14479dd8
GaBaljaintheroom <junsu1222@naver.com>
2023-08-12 21:47:30 +0900

    refactor: 결과 메시지 상수 처리

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -5,6 +5,10 @@ package bridge;
  */
 public class OutputView {
 
+    private static final String RESULT_GAME_MESSAGE = "최종 게임 결과";
+    private static final String WHETHER_GAME_SUCCESS = "게임 성공 여부: ";
+    private static final String ALL_ATTEMPT_COUNT = "총 시도한 횟수: ";
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -20,9 +24,9 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult(GameResult gameResult) {
-        System.out.println("최종 게임 결과");
+        System.out.println(RESULT_GAME_MESSAGE);
         System.out.println(gameResult.getBridgeStatus());
-        System.out.println("게임 성공 여부: " + gameResult.getGameClear());
-        System.out.println("총 시도한 횟수: " + gameResult.getAttemptCount());
+        System.out.println(WHETHER_GAME_SUCCESS + gameResult.getGameClear());
+        System.out.println(ALL_ATTEMPT_COUNT + gameResult.getAttemptCount());
     }
 }
