be788da45bfdada1777a53dc652650d58b9a120f
GaBaljaintheroom <junsu1222@naver.com>
2023-08-12 13:13:39 +0900

    feat: 게임 시작 메서드 작성

--- src/main/java/bridge/BridgeGameController.java
+++ src/main/java/bridge/BridgeGameController.java
@@ -3,14 +3,34 @@ package bridge;
 public class BridgeGameController {
 
     private final InputManager inputManager;
+    private final GameResult gameResult;
 
     public BridgeGameController() {
         this.inputManager = new InputManager();
+        this.gameResult = new GameResult();
     }
 
     public void gameStart(){
         BridgeGame bridgeGame = new BridgeGame(inputManager.makeBridge());
-        inputManager.activeMoveBridge(bridgeGame);
+        boolean isProceed = true;
+        while (isProceed){
+            gameResult.upAttemptCount();
+            if(inputManager.isClearMoveBridge(bridgeGame, gameResult)){
+                gameResult.printGameResult();
+                break;
+            }
+            isProceed = isRetryGame(bridgeGame);
+        }
+    }
 
+    private boolean isRetryGame(BridgeGame bridgeGame) {
+        boolean isRetryGame = inputManager.isRetryGame(bridgeGame);
+        if(isRetryGame){
+            gameResult.restartGameSet();
+            return true;
+        }
+        gameResult.GameClearFail();
+        gameResult.printGameResult();
+        return false;
     }
 }
