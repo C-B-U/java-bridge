07a73dcfee0198d630345127518d61e64b7d5e76
GaBaljaintheroom <junsu1222@naver.com>
2023-08-12 13:13:21 +0900

    feat: 게임 결과 객체 생성

---
+++ src/main/java/bridge/GameResult.java
@@ -0,0 +1,47 @@
+package bridge;
+
+public class GameResult {
+
+    private final BridgeStatus bridgeStatus = new BridgeStatus();
+    private final OutputView outputView = new OutputView();
+    private int attemptCount = 1;
+    private String gameClear = "성공";
+
+    public void printFailBridge(String bridgeMoveStep){
+        bridgeStatus.failStair(bridgeMoveStep);
+        outputView.printMap(bridgeStatus);
+    }
+
+    public void printSuccessBridge(String bridgeMoveStep){
+        bridgeStatus.successStair(bridgeMoveStep);
+        outputView.printMap(bridgeStatus);
+    }
+
+    public void restartGameSet(){
+        bridgeStatus.reset();
+    }
+
+    public void upAttemptCount(){
+        attemptCount++;
+    }
+
+    public void printGameResult(){
+        outputView.printResult(this);
+    }
+
+    public void GameClearFail(){
+        gameClear = "실패";
+    }
+
+    public BridgeStatus getBridgeStatus() {
+        return bridgeStatus;
+    }
+
+    public String getGameClear() {
+        return gameClear;
+    }
+
+    public int getAttemptCount() {
+        return attemptCount;
+    }
+}
