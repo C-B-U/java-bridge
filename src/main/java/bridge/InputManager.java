d6b294f0ab7c9aab6144c9415c5e1b3b37f483ec
GaBaljaintheroom <junsu1222@naver.com>
2023-08-12 13:12:11 +0900

    refactor: 다리 이동 메서드 수정

--- src/main/java/bridge/InputManager.java
+++ src/main/java/bridge/InputManager.java
@@ -1,46 +1,37 @@
 package bridge;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class InputManager {
 
     private final InputView inputView;
     private final BridgeMaker bridgeMaker;
-    private final BridgeStatus bridgeStatus;
 
     public InputManager() {
         this.inputView = new InputView();
         this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        this.bridgeStatus = new BridgeStatus();
     }
 
     public List<String> makeBridge(){
-        System.out.println("다리의 길이를 입력해주세요.");
-        int bridgeSize = inputView.inputBridgeSize(Console.readLine());
+        int bridgeSize = inputView.inputBridgeSize();
         return bridgeMaker.makeBridge(bridgeSize);
     }
 
-    public void activeMoveBridge(BridgeGame bridgeGame){
-        int count = 0;
-        while (++count < bridgeGame.times()){
-            System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-            String bridgeMoveStep = inputView.inputBridgeMoveStep(Console.readLine());
-            boolean isProceed = bridgeGame.move(bridgeMoveStep, count);
+    public boolean isClearMoveBridge(BridgeGame bridgeGame, GameResult gameResult){
+        for (int gameStage = 0; gameStage < bridgeGame.times(); gameStage++){
+            String bridgeMoveStep = inputView.inputBridgeMoveStep();
+            boolean isProceed = bridgeGame.move(bridgeMoveStep, gameStage);
             if (!isProceed){
-                bridgeStatus.failStair(bridgeMoveStep);
-                bridgeStatus.reset();
-                if(checkRetryGame(bridgeGame)){
-                    break;
-                }
+                gameResult.printFailBridge(bridgeMoveStep);
+                return false;
             }
-            bridgeStatus.successStair(bridgeMoveStep);
+            gameResult.printSuccessBridge(bridgeMoveStep);
         }
-
+        return true;
     }
 
-    public boolean checkRetryGame(BridgeGame bridgeGame){
-        String command = inputView.inputGameCommand(Console.readLine());
+    public boolean isRetryGame(BridgeGame bridgeGame){
+        String command = inputView.inputRetryCommand();
         return bridgeGame.retry(command);
     }
 
