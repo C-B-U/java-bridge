0c94cf9d7e45b74eb804a178260e008c1be11fe3
김승진 <ohksj77@gmail.com>
2023-08-13 23:51:17 +0900

    feat(ErrorMessage): 에러 메시지 열거형 추가
    열거형 추가
    - ErrorMessage

---
+++ src/main/java/bridge/ErrorMessage.java
@@ -0,0 +1,19 @@
+package bridge;
+
+public enum ErrorMessage {
+    NOT_NUMERIC_INPUT("숫자만 입력 가능합니다."),
+    BLANK_INPUT("입력값이 비어있습니다."),
+
+    ;
+    private final String message;
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return ERROR_PREFIX + this.message;
+    }
+}
