cef536f173bc4fa6b62f5b4abde37d640a13f89c
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-13 22:54:21 +0900

    feat(StateChecker): 게임이 끝났는지 판단하는 메서드 구현

--- src/main/java/bridge/StateChecker.java
+++ src/main/java/bridge/StateChecker.java
@@ -5,6 +5,13 @@ import java.util.List;
 import static bridge.SuccessFailure.*;
 
 public class StateChecker {
+    public boolean isFinished(List<String> bridge, List<String> player) {
+        if (isAlive(bridge, player) && (player.size() < bridge.size()))
+            return false;
+
+        return !isAlive(bridge, player) || player.size() == bridge.size();
+    }
+
     public boolean isAlive(List<String> bridge, List<String> player) {
         int nowIdx = player.size() - 1;
         String nowBridge = bridge.get(nowIdx);
