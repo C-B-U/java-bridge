26cc6b866297d7d0ae4f8fecc376dee423bf6e98
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-13 22:25:59 +0900

    feat(OutputView): 게임 진행 문구를 출력한다

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -4,20 +4,26 @@ package bridge;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    public void printGameStartMessage() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
 
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+    public void printBridgeLengthInputMessage() {
+        System.out.println("\n다리의 길이를 입력해주세요.");
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printMap() {
     }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+    public void printBridgeSpaceInputMessage() {
+        System.out.println("\n이동할 칸을 선택해주세요. (위: U, 아래: D)");
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
     }
-}
+
+    public void printRetryMessage() {
+        System.out.println("\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
