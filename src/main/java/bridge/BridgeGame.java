dfb26a08d97a0267da5d2dfed3d258fc088ed0c7
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-13 22:28:38 +0900

    feat(BridgeGame): 입력받은 길이에 맞춰 다리를 생성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -4,12 +4,19 @@ package bridge;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private static final OutputView outputView = new OutputView();
+    private static final BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
-     * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    public void start() {
+        try {
+            outputView.printGameStartMessage();
+            bridge = bridgeMaker.makeBridge(inputView.readBridgeSize());
+            move();
+        } catch (IllegalArgumentException error) {
+            outputView.printErrorMessage(error);
+        }
+    }
+
     public void move() {
     }
 
