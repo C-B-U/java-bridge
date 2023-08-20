a909b76791dd3e95e3f32d1bdadc150ef2390334
GaBaljaintheroom <junsu1222@naver.com>
2023-08-21 02:02:56 +0900

    refactor: GameResultManager 필요없는 책임 삭제

--- src/main/java/bridge/controller/GameResultManager.java
+++ src/main/java/bridge/controller/GameResultManager.java
@@ -1,45 +1,19 @@
 package bridge.controller;
 
-import bridge.model.BridgeStatus;
-import bridge.io.view.OutputView;
-
 public class GameResultManager {
 
     private static final String GAME_SUCCESS = "성공";
     private static final String GAME_FAIL = "실패";
-    private final BridgeStatus bridgeStatus = new BridgeStatus();
-    private final OutputView outputView = new OutputView();
     private int attemptCount = 0;
     private String gameClear = GAME_SUCCESS;
 
-    public void printFailBridge(String bridgeMoveStep){
-        bridgeStatus.failStair(bridgeMoveStep);
-        outputView.printMap(bridgeStatus);
-    }
-
-    public void printSuccessBridge(String bridgeMoveStep){
-        bridgeStatus.successStair(bridgeMoveStep);
-        outputView.printMap(bridgeStatus);
-    }
-
-    public void restartGameSet(){
-        bridgeStatus.reset();
-    }
 
     public void upAttemptCount(){
         attemptCount++;
     }
 
-    public void printGameResult(){
-        outputView.printResult(this);
-    }
-
     public void gameClearFail(){
         gameClear = GAME_FAIL;
-    }
-
-    public BridgeStatus getBridgeStatus() {
-        return bridgeStatus;
     }
 
     public String getGameClear() {
