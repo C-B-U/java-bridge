fda10a04a1272c3c6aa454607744880f1a8a05ae
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-21 14:26:52 +0900

    feat(BridgeMaker): 0, 1로 최초 맵을 생성하는 메서드 구현

--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -1,25 +1,22 @@
 package bridge.domain;
 
 import bridge.BridgeNumberGenerator;
+import bridge.Keyword;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-/**
- * 다리의 길이를 입력 받아서 다리를 생성해주는 역할을 한다.
- */
 public class BridgeMaker {
-
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
     }
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
     public List<String> makeBridge(int size) {
-        return null;
+        return IntStream.range(0, size)
+                .mapToObj(bridgeIndex -> Keyword.equalsKey(bridgeNumberGenerator.generate()))
+                .collect(Collectors.toList());
     }
 }
