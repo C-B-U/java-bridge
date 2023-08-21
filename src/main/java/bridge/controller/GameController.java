b81c6ac1c402844fe42203729cb9e35b54e9da42
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-21 14:19:49 +0900

    feat(GameController): 다리 길이 입력 요청하는 메서드 구현

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -4,11 +4,21 @@ import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class GameController {
+    BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private final AttemptCount attemptCount = new AttemptCount();
     private final InputView inputView;
     private final OutputView outputView;
+    private BridgeGame bridgeGame;
 
-    public GameController(InputView inputView, OutputView outputView) {
+    public GameController (InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+    }
+
+    public void setGame() {
+        outputView.printGameStartMessage();
+        outputView.printBridgeSizeInputMessage();
+        bridgeGame = new BridgeGame(bridgeMaker.makeBridge(inputView.readBridgeSize()));
+        processGame();
     }
 }
