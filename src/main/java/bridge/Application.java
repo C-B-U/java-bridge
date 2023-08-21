ae1b46af70f3ce38a1737376e9717295bfc6a4c1
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-21 14:12:29 +0900

    feat(Application): 게임 실행 구현

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,12 @@
 package bridge;
 
-public class Application {
+import bridge.controller.GameController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
 
+public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameController gameController = new GameController(new InputView(), new OutputView());
+        gameController.setGame();
     }
 }
