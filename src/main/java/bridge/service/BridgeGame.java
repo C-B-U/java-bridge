44de122d3e7ae5a9e55828f69a1de03a325d37bf
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-21 14:29:37 +0900

    feat(BridgeGame): 플레이어의 이동을 확인하고 기록하는 메서드 구현

--- src/main/java/bridge/service/BridgeGame.java
+++ src/main/java/bridge/service/BridgeGame.java
@@ -4,20 +4,28 @@ package bridge.service;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private final Bridge bridge;
+    private final GameState gameState = new GameState();
+    private List<String> player = new ArrayList<>();
+    private BridgeMap bridgeMap = new BridgeMap();
+    private PlayerPosition playerPosition = new PlayerPosition();
 
-    /**
-     * 사용자가 칸을 이동할 때 사용하는 메서드
-     * <p>
+    public BridgeGame(List<String> bridge) {
+        this.bridge = new Bridge(bridge);
+        System.out.println(this.bridge.getBridge());
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void move() {
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-     * <p>
-     * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    public boolean move(String moving) {
+        playerPosition.moveNextPosition();
+        player.add(moving);
+        boolean isRightMoving = gameState.isAlive(bridge.getBridge(), playerPosition.getPosition(), moving);
+        boolean isFinished = gameState.isFinished(bridge.getBridge(), player);
+        bridgeMap.addMovingResult(moving, isRightMoving);
+        return isRightMoving && !isFinished;
+    }
     public void retry() {
     }
 }
