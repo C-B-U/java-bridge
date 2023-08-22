05d0b3010b8387a6ea5c875bab2b0b2afdb13b08
김연진 <love_homebody@naver.com>
2023-08-22 16:32:17 +0900

    test(BridgeMaker): 다리 생성 기능 테스트

---
+++ src/test/java/bridge/model/BridgeMakerTest.java
@@ -0,0 +1,35 @@
+package bridge.model;
+
+import bridge.BridgeMaker;
+import bridge.BridgeNumberGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
+
+public class BridgeMakerTest {
+
+    @Test
+    void createBridge() {
+        BridgeNumberGenerator numberGenerator = new BridgeMakerTest.TestNumberGenerator(newArrayList(1, 0, 0));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        List<String> bridge = bridgeMaker.makeBridge(3);
+        assertThat(bridge).containsExactly("U", "D", "D");
+    }
+
+    static class TestNumberGenerator implements BridgeNumberGenerator {
+
+        private final List<Integer> numbers;
+
+        TestNumberGenerator(List<Integer> numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public int generate() {
+            return numbers.remove(0);
+        }
+    }
+}
