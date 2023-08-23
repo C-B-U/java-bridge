0554e3ba60a45fa26ad6164c3e51ee0ec4dd9a03
GaBaljaintheroom <junsu1222@naver.com>
2023-08-24 01:24:27 +0900

    refactor: BRIDGE_GAME_START 메세지 추가

--- src/main/java/bridge/GameProgressMessage.java
+++ src/main/java/bridge/GameProgressMessage.java
@@ -2,6 +2,7 @@ package bridge;
 
 public enum GameProgressMessage {
 
+    BRIDGE_GAME_START("다리 건너기 게임을 시작합니다."),
     INPUT_BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
     INPUT_BRIDGE_STAGE("이동할 칸을 선택해주세요. (위: %s, 아래: %s)");
 
