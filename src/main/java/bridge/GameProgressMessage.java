896cd7120e4d14a1731d72632e05f9c6cd3a54cd
GaBaljaintheroom <junsu1222@naver.com>
2023-08-23 17:23:40 +0900

    feat: GameProgressMessage 이동할 칸 추가

--- src/main/java/bridge/GameProgressMessage.java
+++ src/main/java/bridge/GameProgressMessage.java
@@ -2,8 +2,9 @@ package bridge;
 
 public enum GameProgressMessage {
 
-    INPUT_BRIDGE_LENGTH("다리의 길이를 입력해주세요.");
-    
+    INPUT_BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
+    INPUT_BRIDGE_STAGE("이동할 칸을 선택해주세요. (위: %s, 아래: %s)");
+
     private final String message;
 
     GameProgressMessage(String message) {
