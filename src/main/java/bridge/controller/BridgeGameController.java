369b07c5bd80b2617357e27497a8423039e2f9ab
GaBaljaintheroom <junsu1222@naver.com>
2023-08-24 01:28:28 +0900

    feat: BridgeGameController 작성

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,36 @@
+package bridge.controller;
+
+import bridge.*;
+
+public class BridgeGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeGame bridgeGame;
+
+    public BridgeGameController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.bridgeGame = new BridgeGame();
+    }
+
+    public void letGame(){
+        makeGame();
+        progressGame();
+    }
+
+    private void makeGame() {
+        outputView.printGameStart();
+        int bridgeSize = inputView.readBridgeSize();
+        bridgeGame.makeBridge(bridgeSize);
+    }
+
+    private void progressGame() {
+        while (BridgeStageRecord.isProgressing()){
+            String stage = inputView.readMoving();
+            bridgeGame.move(stage);
+            outputView.printMap(bridgeGame.getBridgeWindowMaker());
+        }
+    }
+
+}
