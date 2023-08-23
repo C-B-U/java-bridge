27eff97393619fe37221863bb60d9c659f91a73a
GaBaljaintheroom <junsu1222@naver.com>
2023-08-23 16:05:26 +0900

    feat: GameCommand 생성

---
+++ src/main/java/bridge/GameCommand.java
@@ -0,0 +1,30 @@
+package bridge;
+
+public enum GameCommand {
+    UP_STAIR("U"),
+    DOWN_STAIR("D");
+
+
+    private final static int  DOWN_RANDOM_NUMBER = 0;
+    private final String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    @Override
+    public String toString() {
+        return command;
+    }
+
+    public static String bridgeStageCase(int num){
+        if (num == DOWN_RANDOM_NUMBER){
+            return DOWN_STAIR.getCommand();
+        }
+        return UP_STAIR.getCommand();
+    }
+}
