3903ace3854d040d4405d21f9216c8ae4de89eac
GaBaljaintheroom <junsu1222@naver.com>
2023-08-23 22:23:04 +0900

    feat: BridgeGame move 작성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -5,12 +5,31 @@ package bridge;
  */
 public class BridgeGame {
 
+    private final BridgeMaker bridgeMaker;
+    private final BridgeWindowMaker bridgeWindowMaker;
+    private Bridge bridge;
+
+    public BridgeGame() {
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridgeWindowMaker = new BridgeWindowMaker();
+    }
+
+    public void makeBridge(int bridge){
+        this.bridge = new Bridge(bridgeMaker.makeBridge(bridge));
+    }
+
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public Bridge move(String bridgeStageCommand) {
+        int curStage = BridgeStageRecord.getCurStage();
+        boolean stageStatus = bridge.isStageStatus(bridgeStageCommand, curStage);
+        bridgeWindowMaker.moveStair(bridgeStageCommand, stageStatus);
+        BridgeStageRecord.nextStage();
+        return bridge;
     }
 
     /**
