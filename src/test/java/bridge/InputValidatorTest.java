6c01772b0757eac8ea1f12c0526b9cdeb93b2a60
GaBaljaintheroom <junsu1222@naver.com>
2023-08-23 17:25:42 +0900

    feat: InputValidatorTest 이동할 칸 테스트

--- src/test/java/bridge/InputValidatorTest.java
+++ src/test/java/bridge/InputValidatorTest.java
@@ -9,6 +9,13 @@ class InputValidatorTest {
     InputValidator inputValidator = new InputValidator();
 
     @Test
+    @DisplayName("다리 길이가 숫자가 아니면 에러가 발생한다.")
+    void validateBridgeIsNumber(){
+        assertThatThrownBy(() -> inputValidator.validateBridgeSize("ㅁ"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     @DisplayName("다리 길이가 3~20사이가 아니면 에러가 발생한다.")
     void validateBridgeLength(){
         assertThatThrownBy(() -> inputValidator.validateBridgeSize("21"))
@@ -16,10 +23,20 @@ class InputValidatorTest {
     }
 
     @Test
-    @DisplayName("다리 길이가 숫자가 아니면 에러가 발생한다.")
-    void validateBridgeIsNumber(){
-        assertThatThrownBy(() -> inputValidator.validateBridgeSize("ㅁ"))
+    @DisplayName("이동할 칸은 대문자 영어가 아니면 에러가 발생한다.")
+    void validateBridgeStageUpperEnglish(){
+        assertThatThrownBy(() -> inputValidator.validateBridgeStage("u"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("이동할 칸은 'U' 혹은 'D'가 아니면 에러가 발생한다.")
+    void validateBridgeStage(){
+        assertThatThrownBy(() -> inputValidator.validateBridgeStage("T"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
 
 }
