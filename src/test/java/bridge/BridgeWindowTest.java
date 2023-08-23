37f0ef8e91cd5ebdd24c16cba81948b8f834e626
GaBaljaintheroom <junsu1222@naver.com>
2023-08-23 22:27:49 +0900

    test: 입력한 칸이 성공했을 때 SUCCESS를 반환하는지 확인

---
+++ src/test/java/bridge/BridgeWindowTest.java
@@ -0,0 +1,15 @@
+package bridge;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+class BridgeWindowTest {
+
+    @Test
+    @DisplayName("입력한 칸이 성공했을 때 SUCCESS를 반환하는지 확인한다.")
+    void getBridgeWindowElement(){
+        BridgeWindowElement bridgeWindowElement = BridgeWindowElement.getStageWindowElement(true);
+        assertThat(bridgeWindowElement).isEqualTo(BridgeWindowElement.SUCCESS);
+    }
+
+}
