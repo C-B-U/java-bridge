55de42f750c005c8ae8dcd29171122a5c1b6aeeb
GaBaljaintheroom <junsu1222@naver.com>
2023-08-23 17:22:32 +0900

    feat: 이동할 칸 ErrorMessage 생성

--- src/main/java/bridge/ErrorMessage.java
+++ src/main/java/bridge/ErrorMessage.java
@@ -1,8 +1,14 @@
 package bridge;
 
 public enum ErrorMessage {
+
+    // bridge
     BRIDGE_LENGTH_ERROR("다리 길이는 %s~%s 사이의 숫자입니다."),
-    INPUT_NOT_NUMBER_ERROR("숫자가 아닙니다.");
+    BRIDGE_STAGE_COMMAND_ERROR("이동할 칸은 %s 혹은 %s 입니다."),
+
+    // global
+    INPUT_NOT_NUMBER_ERROR("숫자가 아닙니다."),
+    INPUT_NOT_UPPER_ENGLISH_ERROR("대문자 영어가 아닙니다.");
 
 
     private static final String error = "[ERROR] ";
