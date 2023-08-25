0ded25d9bbcee4538ddd460cf09b6e60bd24dab8
GaBaljaintheroom <junsu1222@naver.com>
2023-08-25 21:55:12 +0900

    feat: INPUT_BRIDGE_RETRY 게임 다시 시도 여부 추가

--- src/main/java/bridge/GameProgressMessage.java
+++ src/main/java/bridge/GameProgressMessage.java
@@ -4,7 +4,8 @@ public enum GameProgressMessage {
 
     BRIDGE_GAME_START("다리 건너기 게임을 시작합니다."),
     INPUT_BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
-    INPUT_BRIDGE_STAGE("이동할 칸을 선택해주세요. (위: %s, 아래: %s)");
+    INPUT_BRIDGE_STAGE("이동할 칸을 선택해주세요. (위: %s, 아래: %s)"),
+    INPUT_BRIDGE_RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: %s, 종료: %s)");
 
     private final String message;
 
