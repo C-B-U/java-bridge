4c137561af6c2392d690db70257b2825e9b4a394
GaBaljaintheroom <junsu1222@naver.com>
2023-08-26 00:19:32 +0900

    test: main함수 작성

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,12 @@
 package bridge;
 
+import bridge.controller.BridgeGameController;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeGameController bridgeGameController = new BridgeGameController(
+                new InputView(), new OutputView(), new BridgeGame());
+        bridgeGameController.startGame();
     }
 }
