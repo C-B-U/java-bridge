a2bfb1f78a6a7b24dafcae8117c278e2d49d8ec1
김승진 <ohksj77@gmail.com>
2023-08-31 16:36:57 +0900

    feat(ResultManager): 결과 관리 클래스 추가
    클래스 추가
    - ResultManager

---
+++ src/main/java/bridge/manager/ResultManager.java
@@ -0,0 +1,14 @@
+package bridge.manager;
+
+import bridge.constant.MoveCommand;
+import bridge.constant.MoveResult;
+import bridge.constant.ResultStatus;
+import bridge.domain.GameResult;
+
+public class ResultManager {
+    private GameResult gameResult;
+
+    public MoveResult appendResultStatus(final ResultStatus resultStatus, final MoveCommand moveCommand) {
+        return null;
+    }
+}
